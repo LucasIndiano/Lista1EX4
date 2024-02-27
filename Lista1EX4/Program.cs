3d2ef424294c255a58a6666c10c3678d16151d97
ALUNO <ALUNO@LABHPC22>
2024-02-27 16:02:03 -0300

    Adicionar arquivos de projeto.

---
+++ Lista1EX4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1EX4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, soma;
+
+            Console.WriteLine("Digite o numero A: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o numero B: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o numero X: ");
+            x = int.Parse(Console.ReadLine());
+
+            if (x > 5 ) { 
+            soma = (a + b) * x;
+                Console.WriteLine(soma);      
+            }
+            else
+            {
+                soma = (a - b) * x;
+                Console.WriteLine(soma);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
